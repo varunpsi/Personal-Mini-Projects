9cfe0882a5b196f2b3ddf04292d7da53ada01169
Varun Pratap Singh <49983003+varunpsi@users.noreply.github.com>
2020-11-27 01:43:57 +0530

    Add files via upload

---
+++ Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,51 @@
+randomNumber1 = Math.floor(Math.random() * 6) + 1;
+randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+if (randomNumber1 === 1) {
+    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
+}
+else if (randomNumber1 === 2) {
+    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
+}
+else if (randomNumber1 === 3) {
+    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
+}
+else if (randomNumber1 === 4) {
+    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
+}
+else if (randomNumber1 === 5) {
+    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
+}
+else {
+    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+}
+
+if (randomNumber2 === 1) {
+    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
+}
+else if (randomNumber2 === 2) {
+    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
+}
+else if (randomNumber2 === 3) {
+    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
+}
+else if (randomNumber2 === 4) {
+    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
+}
+else if (randomNumber2 === 5) {
+    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
+}
+else {
+    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+}
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector(".container h1").textContent = "⛳Player 1 wins";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector(".container h1").textContent = "Player 2 wins⛳";
+}
+else {
+    document.querySelector(".container h1").textContent = "⛳Draw⛳";
+
+}
