a4b00ab4b2cff32ff17fb95128f9800da0aaed0b
Varun Pratap Singh <49983003+varunpsi@users.noreply.github.com>
2020-11-27 01:59:33 +0530

    Update index.js

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,43 +1,12 @@
-randomNumber1 = Math.floor(Math.random() * 6) + 1;
-randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-if (randomNumber1 === 1) {
-    document.querySelector(".img1").setAttribute("src", "images/dice1.png");
-}
-else if (randomNumber1 === 2) {
-    document.querySelector(".img1").setAttribute("src", "images/dice2.png");
-}
-else if (randomNumber1 === 3) {
-    document.querySelector(".img1").setAttribute("src", "images/dice3.png");
-}
-else if (randomNumber1 === 4) {
-    document.querySelector(".img1").setAttribute("src", "images/dice4.png");
-}
-else if (randomNumber1 === 5) {
-    document.querySelector(".img1").setAttribute("src", "images/dice5.png");
-}
-else {
-    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
-}
+var randomImageSource1 = "images/" + "dice" + randomNumber1 + ".png"
+document.querySelector(".img1").setAttribute("src", randomImageSource1);
 
-if (randomNumber2 === 1) {
-    document.querySelector(".img2").setAttribute("src", "images/dice1.png");
-}
-else if (randomNumber2 === 2) {
-    document.querySelector(".img2").setAttribute("src", "images/dice2.png");
-}
-else if (randomNumber2 === 3) {
-    document.querySelector(".img2").setAttribute("src", "images/dice3.png");
-}
-else if (randomNumber2 === 4) {
-    document.querySelector(".img2").setAttribute("src", "images/dice4.png");
-}
-else if (randomNumber2 === 5) {
-    document.querySelector(".img2").setAttribute("src", "images/dice5.png");
-}
-else {
-    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
-}
+var randomImageSource2 = "images/" + "dice" + randomNumber2 + ".png"
+document.querySelector(".img2").setAttribute("src", randomImageSource2);
+
 
 if (randomNumber1 > randomNumber2) {
     document.querySelector(".container h1").textContent = "⛳Player 1 wins";
